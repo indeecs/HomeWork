546bc64d9983be40b680c720de0c2143a505c679
indeecs <166256206+indeecs@users.noreply.github.com>
2024-04-14 19:59:29 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,9 @@
+immutable_var = 11, True, "Hello", 2.5
+print(immutable_var)
+#immutable_var [1] = False  #Изменить элементы нельзя, т.к. кортеж не поддерживает обращение по элементам(неизменяемая упорядоченная коллекция)
+mutable_list = [12, False, "Python", 3.7]
+mutable_list[0] = 23
+mutable_list[1] = True
+mutable_list[2] = "Cobra"
+mutable_list[3] = 4.5
+print(mutable_list)
