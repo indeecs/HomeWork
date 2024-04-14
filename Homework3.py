6023f620954c15d6f90ee508c80f8a24d943ffb3
indeecs <166256206+indeecs@users.noreply.github.com>
2024-04-14 18:53:49 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Введите словосочетание: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[:1])
+print(my_string[1])
