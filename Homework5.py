0df56ba63924cb6691a0d9ff1219cac1aad0e53c
indeecs <166256206+indeecs@users.noreply.github.com>
2024-04-16 23:59:51 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,11 @@
+my_list = [ 'kiwi', 'apple', 'orange', 'banana']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:6])
+my_list[2] = 'coconut'
+print(my_list)
+my_dict = {'apple':'яблоко', 'coconut':'кокос', 'orange':'апельсин'}
+print(my_dict)
+print("my_dict['apple'] =", my_dict['apple'])
+my_dict['potato'] = 'картофель'
+print(my_dict)
